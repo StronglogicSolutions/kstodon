0eb18d487ea3c9f5972c99228d24c6d13fa260c9
logicp <logicp@logicp.ca>
2021-01-23 17:48:16 -0500

    changing app interface

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,45 @@
 
 #include "config.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   if (argc < 2)
     throw std::invalid_argument{"KStodon called without arguments"};
 
-  BotStats          stats{};
-  std::string       std_out{};
-  ExecuteConfig     config = ParseRuntimeArguments(argc, argv);
+  kstodon::ExecuteConfig config {kstodon::ParseRuntimeArguments(argc, argv)};
+  kstodon::Bot           bot    {config.username};
+  kstodon::BotStats      stats  {};
+  std::string            std_out{};
 
-  if (config.execute_bot) {
-    kstodon::Bot bot{config.username};
-
+  if (config.execute_bot)
+  {
     std::vector<Conversation> replies = bot.FindReplies();
 
-    for (const Conversation& reply : replies) {
+    for (const Conversation& reply : replies)
+    {
       stats.rx_msg++;
-      if (reply.status.is_valid()) {
+      if (reply.status.is_valid())
+      {
         (bot.ReplyToStatus(reply.status)) ?
           stats.tx_msg++ : stats.tx_err++;
       }
     }
-    std_out += "Bot execution complete:\n" +
-               stats.to_string();
   }
-  else {
-    kstodon::Client   client{config.username};
+  else
+  {
     std::vector<File> files{};
 
-    if (!config.file_paths.empty()) {
+    if (!config.file_paths.empty())
+    {
       for (const auto& path : config.file_paths)
         files.emplace_back(File{path});
     }
-
-    std_out +=
-      (client.PostStatus(Status{config.message}, files)) ?
-        "Post succeeded" :
-        "Post failed";
+      (bot.PostStatus(Status{config.message}, files)) ?
+        stats.tx_msg++ : stats.tx_err++;
   }
+
+  std_out += "Bot execution complete:\n" +
+               stats.to_string();
 
   std::cout << std_out << std::endl;
 
