1614c90572d55ab57b8f3f12bc857360311544a8
logicp <logicp@logicp.ca>
2021-02-04 14:45:04 -0500

    updated

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 third_party/gn/out/gn gen out
 ninja -C out
 
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/kstodon
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/ut_kstodon
+patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/lib/libcpr.so out/kstodon
+patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/lib/libcpr.so out/ut_kstodon
