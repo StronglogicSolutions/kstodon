437436e5284164b5ceff489255e66626c6ed88c2
logicp <adventurist@gmail.com>
2021-04-02 10:08:27 -0400

    updating build script

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 third_party/gn/out/gn gen out
 ninja -C out
 
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/lib/libcpr.so out/kstodon
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/lib/libcpr.so out/ut_kstodon
+patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so.1 out/kstodon
+patchelf --replace-needed libcpr.so "$PWD"/third_party/cpr/build/lib/libcpr.so out/kstodon
+patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so.1 out/libkstodon_lib.so
+patchelf --replace-needed libcpr.so "$PWD"/third_party/cpr/build/lib/libcpr.so out/libkstodon_lib.so
+
