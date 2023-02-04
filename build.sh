5d581aa5e7ccebd1c25222fc223a75161ca8ad80
Emmanuel Buckshi <adventurist@gmail.com>
2023-02-04 12:37:32 -0500

    Update build.sh
    Not patching shared lib

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 third_party/gn/out/gn gen out
 ninja -C out
 
-#patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so.1 out/kstodon
-#patchelf --replace-needed libcpr.so "$PWD"/third_party/cpr/build/lib/libcpr.so out/kstodon
-#patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so.1 out/libkstodon_lib.so
-#patchelf --replace-needed libcpr.so "$PWD"/third_party/cpr/build/lib/libcpr.so out/libkstodon_lib.so
-
+patchelf --replace-needed libcpr.so.1 /usr/local/lib/libcpr.so.1 out/kstodon
